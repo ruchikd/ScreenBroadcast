4755881471ccf1bd7b37a1247a4c050188b272aa
Ruchik Dave <ruchikdave@Ruchiks-MacBook-Pro.local>
2017-12-04 14:40:10 -0800

    Initial Commit

---
+++ ScreenBroadcast/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScreenBroadcast
+//
+//  Created by Ruchik Samir Dave on 12/4/17.
+//  Copyright © 2017 Ruchik Samir Dave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
